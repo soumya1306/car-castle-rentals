36ec6f7b5f8e315e227570ebbfe9cd43e99c78e4
Soumya Ranjan Acharya <soumyaranjan@Soumyas-MacBook-Air.local>
2025-10-23 04:41:24 +0530

    Fix GridLoader import path to use absolute imports for Vercel deployment compatibility

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -8,7 +8,7 @@ import { fetchApi } from "@/utils/api";
 
 import Button from "./Button";
 import CarCard from "./CarCard/CarCard";
-import GridLoader from "./Loaders/GridLoader";
+import GridLoader from "@/components/Loaders/GridLoader";
 import Title from "./Title";
 
 export default function FeaturedSection() {
