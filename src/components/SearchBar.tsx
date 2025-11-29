0ae1d6b14aad437142c3c7d263640f1ab8153939
soumya1306 <carcastlegoa@gmail.com>
2025-11-30 03:27:15 +0530

    Revert "added final changes"
    This reverts commit 6ad008ade5ab2e4ff0341e4c555d40acac5b4cae.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiSearch } from "react-icons/fi";
+import { FiFilter, FiSearch } from "react-icons/fi";
 
 interface SearchBarProps {
   onSearch?: (query: string) => void;
