3335c3dd2ff95ed9bdd9b63e18615d7083a9f0c9
soumya1306 <carcastlegoa@gmail.com>
2025-11-30 03:58:05 +0530

    disbled edge runtime for middleware

--- middleware.ts
+++ middleware.ts
@@ -46,5 +46,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.gif$|.*\\.svg$).*)',
-  ]
+  ],
+  runtime:  'nodejs',
 };
